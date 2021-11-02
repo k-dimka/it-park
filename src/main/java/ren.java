eb40ad6a19df74c39a47eafbefcb27375d6ebcfd
Dimon <k-dimka@internet.ru>
2021-11-02 19:27:17 +0500

    no coment

---
+++ src/main/java/ren.java
@@ -0,0 +1,10 @@
+public class ren {
+    public static void main(String[] args) {
+        int d = 1;
+        int f = 3;
+        int c = (d+f);
+        char e = 'a'+2;
+        System.out.println(e);
+        /*System.out.println(c&e);*/
+    }
+}
