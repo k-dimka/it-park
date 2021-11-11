73142476d851998b7f47dded4043a96c1d53ccfc
work <it@zaosab.com>
2021-11-11 14:34:38 +0500

    V3 оптимальный

---
+++ src/main/java/Lesson2/CalculateV3.java
@@ -0,0 +1,47 @@
+package Lesson2;
+
+import java.util.Scanner;
+public class CalculateV3 {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("1 - Сложение; 2 - Вычетание; 3 - Умножение; 4 - Деление; 5 - Выход из программы");
+            System.out.println("Введите номер нужного действия");
+            Scanner console = new Scanner(System.in);
+            int action = console.nextInt();
+            System.out.println("Введите первое число ");
+            Scanner console1 = new Scanner(System.in);
+            double a = console.nextDouble();
+            System.out.println("Введите второе число");
+            Scanner console2 = new Scanner(System.in);
+            double b = console.nextDouble();
+            if (action == 1) {
+                double answer = a + b;
+                System.out.println("Сумма чисел равна: " + answer + ". Попробуем еще?");
+            } else if (action == 2) {
+                double answer = a - b;
+                System.out.println("Разность чисел равна: " + answer + ". Попробуем еще?");
+            } else if (action == 3) {
+                double answer = a * b;
+                System.out.println("Произведение чисел равна: " + answer + ". Попробуем еще?");
+            } else if (action == 4) {
+                if (b == 0) {
+                    System.out.println("На 0 делить нельзя! Повторите попытку ввода");
+                    Scanner console3 = new Scanner(System.in);
+                    double b1 = console.nextDouble();
+                    double answer = a / b1;
+                    System.out.println("Частное чисел равна: " + answer + ". Попробуем еще?");
+                } else {
+                    double answer = a / b;
+                    System.out.println("Частное чисел равна: " + answer + ". Попробуем еще?");
+                }
+            } else if (action == 5) {
+                System.out.println("До новых встреч");
+                System.exit(0);
+            } else if (action > 5) {    /* && странно, но двойное условие не работает*/
+                System.out.println("Номер действия не определен");
+            }  else if (action < 1) {
+                System.out.println("Номер действия не определен");
+            }
+        }
+    }
+}
