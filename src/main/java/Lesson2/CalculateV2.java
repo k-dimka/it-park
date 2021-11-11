7a372dc5362348e9c0b57f0795649c5414e7def8
work <it@zaosab.com>
2021-11-11 13:24:02 +0500

    V2

--- src/main/java/Lesson2/CalculateV2.java
+++ src/main/java/Lesson2/CalculateV2.java
@@ -1,7 +1,7 @@
 package Lesson2;
 
 import java.util.Scanner;
-public class calculate {
+public class CalculateV2 {
     public static void main(String[] args) {
         while (true) {
             System.out.println("1 - Сложение; 2 - Вычетание; 3 - Умножение; 4 - Деление; 5 - Выход из программы");
@@ -15,7 +15,7 @@ public class calculate {
                 System.out.println("Введите второе целое число");
                 Scanner console2 = new Scanner(System.in);
                 int b = console.nextInt();
-                long answer = summa(a, b);
+                long answer = a + b;
                 System.out.println("Сумма чисел равна: " + answer + ". Попробуем еще?");
             } else if (action == 2) {
                 System.out.println("Введите первое целое число");
@@ -24,7 +24,7 @@ public class calculate {
                 System.out.println("Введите второе целое число");
                 Scanner console2 = new Scanner(System.in);
                 int b = console.nextInt();
-                long answer = subtraction(a, b);
+                long answer = a - b;
                 System.out.println("Разность чисел равна: " + answer + ". Попробуем еще?");
             } else if (action == 3) {
                 System.out.println("Введите первое целое число");
@@ -33,7 +33,7 @@ public class calculate {
                 System.out.println("Введите второе целое число");
                 Scanner console2 = new Scanner(System.in);
                 int b = console.nextInt();
-                long answer = multiplication(a, b);
+                long answer = a * b;
                 System.out.println("Произведение чисел равна: " + answer + ". Попробуем еще?");
             } else if (action == 4) {
                 System.out.println("Введите первое целое число");
@@ -46,10 +46,10 @@ public class calculate {
                     System.out.println("На 0 делить нельзя! Повторите попытку ввода");
                     Scanner console3 = new Scanner(System.in);
                     int b1 = console.nextInt();
-                    long answer = division1(a, b1);
+                    long answer = a / b1;
                     System.out.println("Частное чисел равна: " + answer + ". Попробуем еще?");
                 } else {
-                    long answer = division(a, b);
+                    long answer = a / b;
                     System.out.println("Частное чисел равна: " + answer + ". Попробуем еще?");
                 }
             } else if (action == 5) {
@@ -58,19 +58,5 @@ public class calculate {
             }
         }
     }
-        private static long summa ( int a, int b){
-            return a + b;
-        }
-        private static long subtraction ( int a, int b){
-            return a - b;
-        }
-        private static long multiplication ( int a, int b){
-            return a * b;
-        }
-        private static long division ( int a, int b){
-            return a / b;
-        }
-        private static long division1 ( int a, int b){
-            return a / b;
-        }
+
 }
