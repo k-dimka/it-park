3d6885d38d40cd53236c41463ae441a0b2705dc7
work <it@zaosab.com>
2021-11-11 12:22:47 +0500

    Разложил по папкам

--- src/main/java/FistProgram.java
+++
@@ -1,5 +0,0 @@
-public class FistProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/main/java/Lesson1/FistProgram.java
@@ -0,0 +1,7 @@
+package Lesson1;
+
+public class FistProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
