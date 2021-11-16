6f54f2168e3169808a7b8fe4094a3706ce8919e4
work <it@zaosab.com>
2021-11-16 15:40:20 +0500

    задание 5 Фибоначи

---
+++ src/main/java/Lesson5/fibonacci.java
@@ -0,0 +1,23 @@
+package Lesson5;
+
+import java.util.Scanner;
+public class fibonacci {
+    public static void main(String[] args) {
+        System.out.println("Вычисление значения в последовательности Фибоначчи");
+        System.out.print("Введите число N последовалтельностей:");
+        Scanner console = new Scanner(System.in);
+        int N = console.nextInt();
+        int a = 1;
+        if (N>0) {
+            int b = 1;
+            N = N-1;
+            while(N>0) {
+                int c = a+b;
+                N = N-1;
+                a = b;
+                b = c;
+                System.out.print(a+" ");
+            }
+        }
+    }
+}
