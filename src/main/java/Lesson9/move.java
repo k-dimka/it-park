601e982d43996b957a81c1c6ab21f3d9b8ea5cc9
k-dimka <93443412+k-dimka@users.noreply.github.com>
2022-01-09 14:41:21 +0500

    lesson9.1

--- src/main/java/Lesson9/move.java
+++ src/main/java/Lesson9/move.java
@@ -8,14 +8,14 @@ import java.util.Random;
 public class move {
 
     public static void main(String[] args) {
-        Car Lada = new Car("Lada", "Car");
-        Truck Ford = new Truck("Ford", "Truck");
-        Car KIA = new Car("KIA", "Car");
-        Truck Man = new Truck("Man", "Truck");
-        Car Audi = new Car("Audi", "Car");
-        Truck Kamaz = new Truck("Kamaz", "Truck");
-        Car Mazda = new Car("Mazda", "Car");
-        Truck Mersedes = new Truck("Mersedes", "Truck");
+        Automobile Lada = new Car("Lada", "Car");
+        Automobile Ford = new Truck("Ford", "Truck");
+        Automobile KIA = new Car("KIA", "Car");
+        Automobile Man = new Truck("Man", "Truck");
+        Automobile Audi = new Car("Audi", "Car");
+        Automobile Kamaz = new Truck("Kamaz", "Truck");
+        Automobile Mazda = new Car("Mazda", "Car");
+        Automobile Mersedes = new Truck("Mersedes", "Truck");
 
         Automobile[] avto = new Automobile[]{Lada, Ford, KIA, Man, Audi, Kamaz, Mazda, Mersedes};
         for (Automobile car : avto) {
