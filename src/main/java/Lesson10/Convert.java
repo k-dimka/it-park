e550f6b5e10104fe26679581330a44b4eec0f237
work <it@zaosab.com>
2022-01-13 12:40:17 +0500

    всё

--- src/main/java/Lesson10/Convert.java
+++ src/main/java/Lesson10/Convert.java
@@ -1,7 +1,36 @@
 package Lesson10;
 
-public class Convert implements Convertable {
+public class Convert implements Convertable, Inputable {
     public Convert() {
     }
 
+    @Override
+    public void convertCtoK(double value) {
+        Convertable.super.convertCtoK(value);
+    }
+
+    @Override
+    public void convertCtoF(double value) {
+        Convertable.super.convertCtoF(value);
+    }
+
+    @Override
+    public void convertKtoC(double value) {
+        Convertable.super.convertKtoC(value);
+    }
+
+    @Override
+    public void convertFtoC(double value) {
+        Convertable.super.convertFtoC(value);
+    }
+
+    @Override
+    public void convertFtoK(double value) {
+        Convertable.super.convertFtoK(value);
+    }
+
+    @Override
+    public void convertKtoF(double value) {
+        Convertable.super.convertKtoF(value);
+    }
 }
