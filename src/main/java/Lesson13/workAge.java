a5a13ef6f233e0db75a2cd548d057395c36970ea
work <it@zaosab.com>
2022-01-19 16:58:07 +0500

    lesson 13.12

--- src/main/java/Lesson13/workAge.java
+++ src/main/java/Lesson13/workAge.java
@@ -7,12 +7,12 @@ public class workAge {
 
     public String calcYear(String startDate, int Age) {
 
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate startWork = LocalDate.parse(startDate, format);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy"); //задаю какой формат даты использую
+        LocalDate startWork = LocalDate.parse(startDate, format); //получаю дату парсим строку в дату
         LocalDate now = LocalDate.now();
-        startWork.getYear();
-        int nowDate = now.getYear();
-        int stDate = startWork.getYear();
+        int stDate = startWork.getYear(); //беру только год от парсинга, интую
+        int nowDate = now.getYear(); //тоже с текущей даты
+
         if ((nowDate - Age) >= stDate) {
             System.out.println("хватает");
         } else if ((nowDate - Age) < stDate) {
