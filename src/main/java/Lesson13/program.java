fca9c6c8c40574760bb9136df65d293a7057d6ef
work <it@zaosab.com>
2022-01-19 16:49:50 +0500

    lesson 13.1

--- src/main/java/Lesson13/program.java
+++ src/main/java/Lesson13/program.java
@@ -2,8 +2,9 @@ package Lesson13;
 
 public class program extends workAge {
     public static void main(String[] args) {
-        workAge workAge = new workAge();
-        workAge.calcYear("25.01.2014", 5);
+        workAge Age = new workAge();
+        Age.calcYear("25.01.2012", 10);
+
 
     }
 
--- src/main/java/Lesson13/workAge.java
+++ src/main/java/Lesson13/workAge.java
@@ -5,34 +5,19 @@ import java.time.format.DateTimeFormatter;
 
 public class workAge {
 
-    /*private String startDate;
-    private int Age;
-
-    public workAge(String startDate, int age) {
-        this.startDate = startDate;
-        Age = age;
-    }*/
-
-    public workAge() {
-    }
-
-    static boolean calcYear(String startDate, int Age) {
+    public String calcYear(String startDate, int Age) {
 
         DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate startWork = LocalDate.parse(startDate, format);
         LocalDate now = LocalDate.now();
         startWork.getYear();
-        //now = LocalDate.now();
         int nowDate = now.getYear();
         int stDate = startWork.getYear();
-        if ((nowDate - Age) > stDate) {
-            return false;
-       /* } else if ((nowDate - Age) <= stDate) {
-            return true;
-        }*/
-
-
+        if ((nowDate - Age) >= stDate) {
+            System.out.println("хватает");
+        } else if ((nowDate - Age) < stDate) {
+            System.out.println("мало");
         }
-
-        return false;
+        return startDate;
     }
+}
